9aa97cfd8730a00d946a406348c966a87b9bf117
Emile Steenkamp <emile@exploreshackle.com>
2023-07-24 10:28:42 +0200

    init

---
+++ deployment/target/generated-sources/annotations/org/chop/quarkus/scripting/deployment/QuarkusScriptingProcessor$$accessor.java
@@ -0,0 +1,7 @@
+package org.chop.quarkus.scripting.deployment;
+public final class QuarkusScriptingProcessor$$accessor {
+    private QuarkusScriptingProcessor$$accessor() {}
+    public static Object construct() {
+        return new QuarkusScriptingProcessor();
+    }
+}
